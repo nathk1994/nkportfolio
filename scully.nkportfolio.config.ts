e7c1546edc44c7646b0539c40042073ddadfdae9
Nathan Keogh <72452040+nathk1994@users.noreply.github.com>
2023-10-03 19:05:57 +0100

    Upload of portfolio
    First stage of portfolio complete.

---
+++ scully.nkportfolio.config.ts
@@ -0,0 +1,8 @@
+import { ScullyConfig } from '@scullyio/scully';
+export const config: ScullyConfig = {
+  projectRoot: "./src",
+  projectName: "nkportfolio",
+  outDir: './dist/static',
+  routes: {
+  }
+};
